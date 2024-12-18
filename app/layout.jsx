130c56049fa7642c5776202ef480b54a0027424a
Prajwal Gopal Poojary <prajwalgpy@gmail.com>
2024-12-18 11:13:12 +0530

    first commit

---
+++ app/layout.jsx
@@ -0,0 +1,22 @@
+import "./globals.css";
+
+import Footer from "../components/footer";
+
+export const metadata = {
+  title: "Prajwal GP",
+  description: "A personal portfolio ",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`antialiased bg-black`}
+        style={{ fontFamily: "SF Pro Display Bold, sans-serif" }}
+      >
+        {children}
+        <Footer />
+      </body>
+    </html>
+  );
+}
